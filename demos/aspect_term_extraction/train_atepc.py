fc09946d7116da53a68a34ffa5c93a02cc743a96
radya13rahman <radyaamirur@gmail.com>
2022-06-03 14:34:34 +0700

    Update train_atepc.py

--- demos/aspect_term_extraction/train_atepc.py
+++ demos/aspect_term_extraction/train_atepc.py
@@ -25,9 +25,9 @@ atepc_config = ATEPCConfigManager.get_atepc_config_english()
 atepc_config.pretrained_bert = 'microsoft/deberta-v3-base'
 atepc_config.lcf = 'cdm'
 atepc_config.model = ATEPCModelList.FAST_LCF_ATEPC
-atepc_config.optimizer = 'adagrad'
-atepc_config.learning_rate = 0.000002
-atepc_config.num_epoch = 20
+atepc_config.l2reg = 0.00000001
+atepc_config.batch_size = 32
+atepc_config.num_epoch = 30
 dataset_path = DatasetItem('100.CustomDataset')
 # or your local dataset: dataset_path = 'your local dataset path'
 
