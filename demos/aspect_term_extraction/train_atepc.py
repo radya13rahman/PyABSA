7f329bcddb01e3df3b94750876a51b6140c59c4c
radya13rahman <radyaamirur@gmail.com>
2022-06-06 00:49:57 +0700

    Update train_atepc.py

--- demos/aspect_term_extraction/train_atepc.py
+++ demos/aspect_term_extraction/train_atepc.py
@@ -22,12 +22,12 @@ import torch
 
 atepc_config = ATEPCConfigManager.get_atepc_config_english()
 
-atepc_config.pretrained_bert = 'microsoft/mdeberta-v3-base'
+atepc_config.pretrained_bert = 'microsoft/deberta-v3-base'
 atepc_config.lcf = 'cdm'
 # atepc_config.hidden_dim = 1024
 # atepc_config.embed_dim = 1024
 atepc_config.model = ATEPCModelList.FAST_LCF_ATEPC
-# atepc_config.num_epoch = 20
+atepc_config.num_epoch = 20
 dataset_path = DatasetItem('100.CustomDataset')
 # or your local dataset: dataset_path = 'your local dataset path'
 
