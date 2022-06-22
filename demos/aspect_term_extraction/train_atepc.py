6f9e014a5b157cc016b0ae645a86223efe22f0b8
radya13rahman <radyaamirur@gmail.com>
2022-06-22 07:51:00 +0700

    Update train_atepc.py

--- demos/aspect_term_extraction/train_atepc.py
+++ demos/aspect_term_extraction/train_atepc.py
@@ -24,10 +24,10 @@ import pandas as pd
 
 atepc_config = ATEPCConfigManager.get_atepc_config_english()
 
-atepc_config.pretrained_bert = 'microsoft/deberta-v3-base'
+atepc_config.pretrained_bert = 'microsoft/deberta-v3-large'
 atepc_config.lcf = 'cdm'
-# atepc_config.hidden_dim = 1024
-# atepc_config.embed_dim = 1024
+atepc_config.hidden_dim = 1024
+atepc_config.embed_dim = 1024
 atepc_config.model = ATEPCModelList.FAST_LCF_ATEPC
 atepc_config.num_epoch = 10
 dataset_path = DatasetItem('100.CustomDataset')
