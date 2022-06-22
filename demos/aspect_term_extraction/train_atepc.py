8bb24049d8c1807b0d34ab067f08083124479155
radya13rahman <radyaamirur@gmail.com>
2022-06-22 14:47:51 +0700

    Update train_atepc.py

--- demos/aspect_term_extraction/train_atepc.py
+++ demos/aspect_term_extraction/train_atepc.py
@@ -22,31 +22,31 @@ from pyabsa.functional.dataset import DatasetItem
 import torch
 import pandas as pd
 
-atepc_config = ATEPCConfigManager.get_atepc_config_english()
+# atepc_config = ATEPCConfigManager.get_atepc_config_english()
 
-atepc_config.pretrained_bert = 'microsoft/deberta-v3-large'
-atepc_config.lcf = 'cdm'
-atepc_config.hidden_dim = 1024
-atepc_config.embed_dim = 1024
-atepc_config.model = ATEPCModelList.FAST_LCF_ATEPC
-atepc_config.num_epoch = 10
-dataset_path = DatasetItem('100.CustomDataset')
+# atepc_config.pretrained_bert = 'microsoft/deberta-v3-large'
+# atepc_config.lcf = 'cdm'
+# atepc_config.hidden_dim = 1024
+# atepc_config.embed_dim = 1024
+# atepc_config.model = ATEPCModelList.FAST_LCF_ATEPC
+# atepc_config.num_epoch = 10
+# dataset_path = DatasetItem('100.CustomDataset')
 # or your local dataset: dataset_path = 'your local dataset path'
 
 # for f in findfile.find_cwd_files(['.augment.ignore'] + dataset_path):
 #     os.rename(f, f.replace('.augment.ignore', '.augment'))
 
-aspect_extractor = ATEPCTrainer(config=atepc_config,
-                                dataset=dataset_path,
-                                from_checkpoint='',  # set checkpoint to train on the checkpoint.
-                                checkpoint_save_mode=1,
-                                auto_device=True
-                                ).load_trained_model()
+# aspect_extractor = ATEPCTrainer(config=atepc_config,
+#                                 dataset=dataset_path,
+#                                 from_checkpoint='',  # set checkpoint to train on the checkpoint.
+#                                 checkpoint_save_mode=1,
+#                                 auto_device=True
+#                                 ).load_trained_model()
 
 # checkpoint_map = available_checkpoints(from_local=True)
-# aspect_extractor = ATEPCCheckpointManager.get_aspect_extractor(checkpoint='/content/gdrive/MyDrive/Tugas_akhir/Rombak_model/PyABSA/demos/aspect_term_extraction/checkpoints/fast_lcf_atepc_100.CustomDataset_cdm_apcacc_87.92_apcf1_80.88_atef1_71.08',
-#                                                                auto_device=True  # False means load model on CPU
-#                                                                )
+aspect_extractor = ATEPCCheckpointManager.get_aspect_extractor(checkpoint='/content/gdrive/MyDrive/Tugas_akhir/Rombak_model/PyABSA/demos/aspect_term_extraction/checkpoints/fast_lcf_atepc_100.CustomDataset_cdm_apcacc_83.33_apcf1_69.26_atef1_64.8',
+                                                               auto_device=True  # False means load model on CPU
+                                                               )
 
 examples = ['But the staff was so nice to us .',
             'But the staff was so horrible to us .',
@@ -64,7 +64,7 @@ for x in df['comment']:
     review_list.append(x)
 
 inference_source = ABSADatasetList.Laptop14
-atepc_result = aspect_extractor.extract_aspect(inference_source=examples,
+atepc_result = aspect_extractor.extract_aspect(inference_source=review_list,
                                                save_result=True,
                                                print_result=True,  # print the result
                                                pred_sentiment=True,  # Predict the sentiment of extracted aspect terms
